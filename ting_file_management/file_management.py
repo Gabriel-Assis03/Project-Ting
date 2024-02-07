ab02cff81d71742112ce3c96b3758f78bc172566
Gabriel do Carmo Assis <gabrieleboltffggff@gmail.com>
2024-02-07 18:06:29 -0300

    req 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import sys
+import os
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not os.path.exists(path_file):
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+        return None
+
+    if not path_file.endswith('.txt'):
+        sys.stderr.write("Formato inválido\n")
+        return None
+
+    with open(path_file, 'r') as file:
+        lines = file.read().split('\n')
+    
+    return lines
