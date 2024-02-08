bddb3a7c6cf5fad78fc116b5469dac44a5d8756f
Gabriel do Carmo Assis <gabrieleboltffggff@gmail.com>
2024-02-07 22:27:54 -0300

    req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,6 +1,23 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+import sys
+# import os
+from ting_file_management.file_management import txt_importer
 
+
+def process(path_file, instance):
+    for ins in instance.queue:
+        if path_file == ins:
+            return None
+    text = txt_importer(path_file)
+    if text == None:
+        return None
+    ret = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(text),
+        "linhas_do_arquivo": text
+    }
+    instance.enqueue(path_file)
+    print(ret)
+    
 
 def remove(instance):
     """Aqui irá sua implementação"""
