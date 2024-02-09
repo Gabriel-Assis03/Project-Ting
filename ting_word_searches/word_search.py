a3599ee15b6eca0195ce63999f872ca1f259f685
Gabriel do Carmo Assis <gabrieleboltffggff@gmail.com>
2024-02-08 23:15:31 -0300

    req 6 caminhando

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,25 @@
+from ting_file_management.file_management import txt_importer
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    arq = instance.queue
+    ret = list()
+    for path in arq:
+        line = list()
+        text = txt_importer(path)
+        for i in range(len(text)):
+            if word.lower() in text[i].lower():
+                line.append({"linha": i})
+        ret.append(
+            {
+                "palavra": word,
+                "arquivo": path,
+                "ocorrencias": line
+            }
+        )
+    return ret
+
+
 
 
 def search_by_word(word, instance):
